50018ad09c9b81ddad0a57ded62d10cfb0622981
llizuna2001ll <issam.fladi@gmail.com>
2023-03-21 10:21:26 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include main.h
+
+/*
+ * print_alphabet - print alphabets
+ * Return:returns nothing
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	for(ch='a';ch <= z; ch++)
+	{
+		_putchar(ch);
+		ch++;
+
+	}
+	_putchar('\n');
+
+}
